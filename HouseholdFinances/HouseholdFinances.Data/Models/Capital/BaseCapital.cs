1961a8efada61e87460f4e93d764ad6b03780d78
Milena Petkanova <milenaslaveikova78@gmail.com>
2019-02-20 18:40:14 +0200

    add data and services projects, create db, controller

---
+++ HouseholdFinances/HouseholdFinances.Data/Models/Capital/BaseCapital.cs
@@ -0,0 +1,13 @@
+﻿namespace FamilyIncomeExpences.Data.Models.Capital
+{
+    public abstract class BaseCapital
+    {
+        public abstract decimal Cash { get; set; }
+
+        public abstract decimal DebitCardFirst { get; set; }
+
+        public abstract decimal DebitCardSecond { get; set; }
+
+        public abstract decimal DebitCardThird { get; set; }
+    }
+}
---
+++ HouseholdFinances/HouseholdFinances.WebApi/Controllers/CapitalsController.cs
@@ -0,0 +1,62 @@
+﻿namespace HouseholdFinances.WebApi.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using HouseholdFinances.Services.DataServices.Contracts;
+    using HouseholdFinances.Services.DataServices.Models.Capital;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CapitalsController : ControllerBase
+    {
+        private readonly ICapitalService _capitalService;
+
+        public CapitalsController(ICapitalService capitalService)
+        {
+            this._capitalService = capitalService;
+        }
+
+        // GET: api/Capitals
+        [HttpGet]
+        public IEnumerable<CapitalDto> Get()
+        {
+            return this._capitalService.GetAll();
+        }
+
+        // GET: api/Capitals/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Capitals
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        // POST: api/Capitals
+        [HttpPost]
+        public CapitalDto Create(CapitalDto capital)
+        {
+             this._capitalService.AddCapital();
+        }
+
+        // PUT: api/Capitals/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
