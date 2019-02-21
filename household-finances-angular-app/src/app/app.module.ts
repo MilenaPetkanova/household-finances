4b893a24fa07c89405374a17338b8f0218e0049f
Milena Petkanova <milenaslaveikova78@gmail.com>
2019-02-21 16:39:45 +0200

    create form, submitting capital record

--- household-finances-angular-app/src/app/app.module.ts
+++ household-finances-angular-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -20,6 +20,7 @@ import { CapitalService } from './shared/capital.service';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [CapitalService],
--- household-finances-angular-app/src/app/capitals/capital/capital.component.ts
+++ household-finances-angular-app/src/app/capitals/capital/capital.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { CapitalService } from './../../shared/capital.service';
 import { NgForm } from '@angular/forms';
+import {FormControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-capital',
@@ -11,20 +13,38 @@ import { NgForm } from '@angular/forms';
 export class CapitalComponent implements OnInit {
 
   public service;
+  public reactiveForm: FormGroup;
 
-  constructor(private _capitalService: CapitalService ) { }
+  constructor(private _formBuilder: FormBuilder, private _capitalService: CapitalService) { }
 
   ngOnInit() {
     this.service = this._capitalService;
+    this.reactiveForm = this._formBuilder.group({
+      cash: [null, Validators.required],
+      debitCardFirst: [null, Validators.required],
+      debitCardSecond: [null, Validators.required],
+      debitCardThird: [null, Validators.required]
+    });
   }
 
-  onSubmit(form: NgForm) {
-    this.insertRecord(form);
-  }
+  get cash() { return this.reactiveForm.get('cash'); }
+  get debitCardFirst() { return this.reactiveForm.get('debitCardFirst'); }
+  get debitCardSecond() { return this.reactiveForm.get('debitCardSecond'); }
+  get debitCardThird() { return this.reactiveForm.get('debitCardThird'); }
+
+  // onSubmit(form: NgForm) {
+  //   this.insertRecord(form);
+  // }
 
   insertRecord(form: NgForm) {
     this._capitalService.postCapital(form.value).subscribe(res => {
       console.log(res);
     });
   }
+
+  onSubmit() {
+    console.log(this.reactiveForm.value);
+
+  }
 }
+
